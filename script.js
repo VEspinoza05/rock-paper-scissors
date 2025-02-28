fb6067d3ea6209294cae19161fa41385e73c552c
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-02-28 09:15:47 -0600

    Add function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+function getComputerChoice() {
+    const minNumber = 1;
+    const maxNumber = 3;
+    const randomNumber = Math.floor(Math.random() * maxNumber + minNumber);
+
+    switch(randomNumber) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors"
+            break;
+    }
+}
